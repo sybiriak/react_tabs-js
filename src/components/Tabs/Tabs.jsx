db724859369fd8ac0eeddaccc23c896c73c914e0
sybiriak <siibiryak@gmail.com>
2024-11-06 13:17:17 +0200

    Add tabs handler

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,48 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const tabChangeHandler = tabId => {
+    if (tabId === activeTabId) {
+      return;
+    }
+
+    onTabSelected(tabId);
+  };
+
+  let activeId = activeTabId;
+
+  if (tabs.findIndex(tab => tab.id === activeTabId) === -1) {
+    activeId = tabs[0].id;
+  }
+
+  const tabContent =
+    tabs.find(tab => tab.id === activeId)?.content ?? tabs[0].content;
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={classNames({ 'is-active': tab.id === activeId })}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => tabChangeHandler(tab.id)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabContent}
+      </div>
+    </div>
+  );
+};
